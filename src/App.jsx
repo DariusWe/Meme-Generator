c104996aaee302d41bd352eb9e452f9f886c8ca5
Darius Weiberg <dariusweiberg@gmail.com>
2022-12-19 00:06:39 +0100

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -17,21 +17,60 @@ function App() {
       });
   }, []);
 
+  const resetActiveImage = (image) => {
+    setActiveImage(image);
+    setTopCaptionValue("");
+    setBottomCaptionValue("");
+  };
+
+  const setUploadedImage = (e) => {
+    const reader = new FileReader();
+    const file = e.target.files[0];
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      const image = new Image();
+      image.crossOrigin = "";
+      image.src = reader.result;
+      image.onload = (e) => {
+        const width = e.target.width;
+        const height = e.target.height;
+        resetActiveImage({
+          url: reader.result,
+          name: file.title,
+          width: width,
+          height: height,
+        });
+      };
+    };
+  };
+
   return (
     <div className="App">
       <div className="image-slider">
         {images &&
           images.map((image) => (
-            <img key={image.id} src={image.url} alt={image.name} onClick={() => setActiveImage(image)} />
+            <img key={image.id} src={image.url} alt={image.name} onClick={() => resetActiveImage(image)} />
           ))}
       </div>
       <div className="bottom-section">
         <Preview image={activeImage} topCaption={topCaptionValue} bottomCaption={bottomCaptionValue} />
         <div className="editing-section">
+          <label htmlFor="uploaded-img">Upload an image:</label>
+          <input type="file" id="uploaded-img" accept=".png, .jpg, .jpeg" onChange={setUploadedImage} />
           <label htmlFor="top-caption">Caption Top:</label>
-          <input id="top-caption" type="text" onChange={(e) => setTopCaptionValue(e.target.value)} />
+          <input
+            id="top-caption"
+            type="text"
+            onChange={(e) => setTopCaptionValue(e.target.value)}
+            value={topCaptionValue}
+          />
           <label htmlFor="bottom-caption">Caption Bottom:</label>
-          <input id="bottom-caption" type="text" onChange={(e) => setBottomCaptionValue(e.target.value)} />
+          <input
+            id="bottom-caption"
+            type="text"
+            onChange={(e) => setBottomCaptionValue(e.target.value)}
+            value={bottomCaptionValue}
+          />
         </div>
       </div>
     </div>
--- src/components/preview.component.jsx
+++ src/components/preview.component.jsx
@@ -4,12 +4,11 @@ import { useRef, useEffect } from "react";
 const Preview = ({ image, topCaption, bottomCaption }) => {
   const canvasRef = useRef(null);
 
-  // image not loading in the beginning
-
   useEffect(() => {
-    if (!canvasRef.current) {
-      return;
-    }
+    canvasRef.current && drawCanvas();
+  }, [topCaption, bottomCaption]);
+
+  const drawCanvas = () => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
     const img = document.getElementById("source");
@@ -18,17 +17,19 @@ const Preview = ({ image, topCaption, bottomCaption }) => {
     ctx.fillStyle = "white";
     ctx.shadowColor = "black";
     ctx.shadowBlur = 5;
-    ctx.fillText(topCaption, 150 - (topCaption.length * 7), 35);
-    ctx.fillText(bottomCaption, 150 - (bottomCaption.length * 7), (image.height * 300 / image.width) - 30);
-    //const dataURL = canvas.toDataURL();
-  }, [image, topCaption, bottomCaption]);
+    ctx.fillText(topCaption, 150 - topCaption.length * 7, 35);
+    ctx.fillText(bottomCaption, 150 - bottomCaption.length * 7, (image.height * 300) / image.width - 30);
+    if (!image.id) {
+      const dataURL = canvas.toDataURL();
+    }
+  };
 
   return (
     <div className="preview-container">
       {image && (
         <div>
           <canvas ref={canvasRef} width={300} height={(image.height / image.width) * 300} />
-          <img style={{ display: "none" }} id="source" src={image.url} alt={image.name} />
+          <img style={{ display: "none" }} id="source" src={image.url} alt={image.name} onLoad={drawCanvas} />
         </div>
       )}
     </div>
